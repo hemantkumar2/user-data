1650b8a87a7e647f787216cde2a330ebd663c8ef
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-03-27 19:15:05 +0530

    removed app file, created components folder and added table component

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AntdTable from "components/AntdTable";
+
+function index() {
+  return (
+    <div>
+      <AntdTable />
+    </div>
+  );
+}
+
+export default index;
